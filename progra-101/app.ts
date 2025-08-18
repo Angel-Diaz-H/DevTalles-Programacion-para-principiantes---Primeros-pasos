94f8bfc8e62663395b0b31c171860f15785c8efd
Angel-Diaz-H <130614039+Angel-Diaz-H@users.noreply.github.com>
2025-08-18 12:59:42 -0600

    ejercicio 1

---
+++ progra-101/app.ts
@@ -0,0 +1,11 @@
+let numberOfLines = 0;
+
+function printLineNumber() {
+    console.log('Línea # ', numberOfLines);
+    numberOfLines++;
+}
+
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
