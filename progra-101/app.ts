9fac34964d41bebe46fd92fe71f4ec7e1f0a0139
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-08-20 11:35:34 -0600

    Ejercicio estructura de control IF

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,1 +1,12 @@
 console.log("Hola Mundo!");
+
+let isTired:boolean = false;
+
+// En Javascript se utiliza triple símbolo de igual para validarlo.
+if (isTired) {
+    console.log("Tomar café");
+} else{
+    console.log("Tomar agua");
+}
+
+console.log("Fin del programa");
