60b4cb5e574e220cec1eb7f3f99ebdfb7b273e99
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-08-27 16:05:47 -0600

    object-literal

---
+++ progra-101/sections/object-literal.ts
@@ -0,0 +1,29 @@
+export let car = {
+    color: 'blue',
+    year: 2020,
+    km: 3000,
+    brand: 'Toyota',
+    gearBox: 'automatic'
+}
+
+let smartTv = {
+    color: 'black',
+    brand: 'Samsung',
+    year: 2025,
+    sizeInches: 90
+}
+
+let youTubeVideo = {
+    description: 'Ejercicio práctico sobre objectos en Typescript',
+    day: 23,
+    month: 12,
+    year: 2022,
+    views: 70300,
+    author: "Angel Díaz",
+    likes: 2300,
+    dislikes: 10
+}
+
+console.log(car);
+console.log(smartTv);
+console.log(youTubeVideo);
