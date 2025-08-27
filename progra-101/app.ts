07712563458e90e0fc502c3277cba9fa851a0ce6
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-08-27 12:49:47 -0600

    arreglos - flowers

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,1 +1,6 @@
-console.log("Hello World");
+let flowers: string[] = ['Rose', 'Sunflower', 'Daisy'];
+
+// .length ayuda a contar la cantidad de elementos de un arreglo, en este caso.
+for (let i:number = 0; i < flowers.length; i++){
+    console.log(flowers[i]);
+}
