91831ce213bc01e06aa46e207d615edc7f86e3c5
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-08-27 11:15:41 -0600

    Import, export - Separar código en varios archivos

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,13 +1,1 @@
-export function greet (name: string = 'Mundo') {
-    console.log('Hello,', name, '!');
-}
-
-// Se puede indicar explícitamente el tipo de dato que retorna.
-// Se puede definir un valor predeterminado a los parámetros.
-function addTwoNumbers(num1: number, num2: number) {
-    return num1 + num2;
-}
-
-let total: number = addTwoNumbers(10, 20);
-
-console.log('Total: ', total);
+console.log("Hola mundooo!");
---
+++ progra-101/sections/import-exports.ts
@@ -0,0 +1,18 @@
+import {
+    addTwoNumbers,
+    substractTwoNumbers,
+    timesTwoNumbers,
+    divideTwoNumbers } from "../../helpers/math-helpers";
+
+let num1 = 30;
+let num2 = 20;
+
+let total1: number = addTwoNumbers(num1, num2);
+let total2: number = substractTwoNumbers(num1, num2);
+let total3: number = timesTwoNumbers(num1, num2);
+let total4: number = divideTwoNumbers(num1, num2);
+
+console.log('Add: ', total1);
+console.log('Substract: ', total2);
+console.log('Times: ', total3);
+console.log('Divide: ', total4);
